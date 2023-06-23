26fa7cb2a9624857d850e4523e5418aef3797b02
Ahyeon Lee <laah0602@gmail.com>
2023-06-23 11:45:58 +0900

    [ADD] upload file

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -13,8 +14,8 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-export const authService = firebase.auth();
-
 export const firebaseInstance = firebase;
 
+export const authService = firebase.auth();
 export const dbService = firebase.firestore();
+export const storageService = firebase.storage();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { dbService } from "../fbase";
+import { v4 as uuidv4 } from "uuid";
+import { dbService, storageService } from "../fbase";
 import { addDoc, collection } from "firebase/firestore";
 import Aweet from "../components/Aweet";
 
 const Home = ({ userObj }) => {
   const [aweet, setAweet] = useState("");
   const [aweets, setAweets] = useState([]);
+  const [attachment, setAttachment] = useState();
 
   useEffect(() => {
     dbService.collection("aweets").onSnapshot((snapshot) => {
@@ -19,12 +21,15 @@ const Home = ({ userObj }) => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    await addDoc(collection(dbService, "aweets"), {
-      text: aweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-    });
-    setAweet("");
+    const fileRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+    const response = await fileRef.putString(attachment, "data_url");
+    console.log(response);
+    // await addDoc(collection(dbService, "aweets"), {
+    //   text: aweet,
+    //   createdAt: Date.now(),
+    //   creatorId: userObj.uid,
+    // });
+    // setAweet("");
   };
 
   const onChange = (event) => {
@@ -33,6 +38,23 @@ const Home = ({ userObj }) => {
     } = event;
     setAweet(value);
   };
+
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      console.log(finishedEvent);
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+  const onClearAttachment = () => setAttachment(null);
 
   return (
     <div>
@@ -44,7 +66,14 @@ const Home = ({ userObj }) => {
           placeholder="What's on your mind?"
           maxLength={120}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Aweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {aweets.map((aweet) => (
@@ -58,4 +87,5 @@ const Home = ({ userObj }) => {
     </div>
   );
 };
+
 export default Home;
