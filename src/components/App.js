f454f9fe1b48d443c5168ebeffe387906c2647b1
Ahyeon Lee <laah0602@gmail.com>
2023-06-24 00:31:41 +0900

    [ADD] update profile

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,45 @@ import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
-    const [init, setInit] = useState(false);
-    const [userObj, setUserObj] = useState(null);
-    useEffect(() => {
-        // 유저 변화 관찰
-        authService.onAuthStateChanged((user) => {
-          if (user) {
-            setUserObj(user);
-          }
-          setInit(true);
+  const [init, setInit] = useState(false);
+  const [userObj, setUserObj] = useState(null);
+  useEffect(() => {
+    // 유저 변화 관찰
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
         });
-      }, []);
-
-    return (
+      } else {
+        setUserObj(null);
+      }
+      setInit(true);
+    });
+  }, []);
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    });
+  };
+  return (
     <>
-    {init? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj}/> : "Initializing.."}
-    {/* <footer>&copy; {new Date().getFullYear()} Awitter</footer> */}
+      {init ? (
+        <AppRouter
+          refreshUser={refreshUser}
+          isLoggedIn={Boolean(userObj)}
+          userObj={userObj}
+        />
+      ) : (
+        "Initializing.."
+      )}
+      {/* <footer>&copy; {new Date().getFullYear()} Awitter</footer> */}
     </>
-    );
+  );
 }
 
 export default App;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { authService } from "../fbase";
 import { useHistory } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ refreshUser, userObj }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+  //   const getMyAweets = async () => {
+  //     const aweets = await dbService
+  //       .collection("aweets")
+  //       .where("creatorId", "==", userObj.uid)
+  //       .orderBy("createdAt")
+  //       .get();
+  //       console.log(aweets.docs.map(doc => doc.data()));
+  //   };
+  //   useEffect(() => {
+  //     getMyAweets();
+  //   }, []);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+      refreshUser();
+    }
+  };
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
